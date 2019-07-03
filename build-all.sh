3900e660b88570e5b76e0f8401d3f9a6399d70ec
Giancarlo Di Paolantonio <giancarlo.dipaolantonio@gmail.com>
2019-07-03 15:28:25 +0200

    We can now build all projects in the right order and the output of each build step is logged and not shown on console

--- build-all.sh
+++ build-all.sh
@@ -4,13 +4,15 @@ ROOT_PATH=`pwd`
 
 ################################################################################
 
+
 echo "[build] Building all projects"
 
-for project in "checkout" "flights" "frontend" "orders"
+START_TIME=`date +%s`
+for project in "frontend" "checkout" "flights" "orders"
 do
   echo "[build] Building project '$project'"
   cd $ROOT_PATH/$project
-  sh ./build.sh
+  sh ./build.sh > ./build.out
   if [ $? != 0 ]
     then
       echo "[build] Failure for project '$project'"
@@ -19,4 +21,6 @@ do
   echo "[build] Success for project '$project'"
 done
 
-echo "[build] Successfully executed all project builds"
+END_TIME=`date +%s`
+EXECUTION_TIME=`expr $END_TIME - $START_TIME`
+echo "[build] Successfully executed all project builds in $EXECUTION_TIME seconds"
